6097f30c0286c7a89d0f0c933ecdf367d7ee08b5
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-11-18 15:25:25 +0100

    Add project files.

---
+++ Time-And-TimePeriod-Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Time_And_TimePeriod_Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Time-And-TimePeriod-Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Time_And_TimePeriod_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
